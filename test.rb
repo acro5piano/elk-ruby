8d42a9dac06b97ecef76ff882c1591f003322909
gosho-kazuya <gosho-kazuya@919.jp>
2016-11-26 18:32:21 +0900

    roop test

--- test.rb
+++ test.rb
@@ -2,7 +2,7 @@ require 'logstash-logger'
 
 logger = LogStashLogger.new(type: :tcp, port: 9600)
 
-100.times do
+while true do
   logger.info 'This is a test.' if rand(1..3) == 1
   logger.error 'Woops! something is wrong.' if rand(1..3) == 1
   logger.fatal 'Call an engineer in office...' if rand(1..3) == 1
